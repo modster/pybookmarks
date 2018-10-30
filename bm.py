66fce1dae280caae1d8aca3614151c37c5e50655
modster <unmodster@gmail.com>
2018-10-30 15:12:41 -0600

    deleted unused method

--- bm.py
+++ bm.py
@@ -1,10 +1,12 @@
+'''
+This program cleans up the bookmarks.html file exported from chrome which uses netscape bookmarks format
+Author: Mike Greeff 
+'''
+
 from bs4 import BeautifulSoup
 
 with open("modster-bookmarks-copy.html") as fp:
     soup = BeautifulSoup(fp , features="html.parser")
-
-def a_row(a_date, a_str, a_href):
-    return f"{a_date}, {a_str}, {a_href}"
 
 a_list = soup.find_all('a')
 
@@ -13,24 +15,3 @@ for each_a in a_list:
     a_str = each_a.string
     a_href = each_a.attrs['href']
     print(a_date, a_str, a_href, sep=",")
-
-    '''
-    fh = open("Hello.txt", "a")
-    write("{a_date}")
-    fh.close()
-
-filename = "hello.txt"
-file = open(filename, "r")
-for line in file:
-   print line,
-
-To append to file, use:
-fh = open("Hello.txt", "a")
-write("Hello World again")
-fh.close()
-
-To close a file, use
-fh = open("hello.txt", "r")
-print fh.read()
-fh.close()
-'''
